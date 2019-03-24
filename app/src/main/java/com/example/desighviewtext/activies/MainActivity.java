d9eddd8fa13e41185b5765c80822f76e9fb4b18b
huangyuanhao <307975180@qq.com>
2019-03-24 23:09:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/desighviewtext/activies/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.desighviewtext.activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.desighviewtext.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/desighviewtext/views/FirstDesignView.java
@@ -0,0 +1,69 @@
+package com.example.desighviewtext.views;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FirstDesignView extends android.support.v7.widget.AppCompatImageView {
+
+    // 这三个构造方法都必须写，否则找不到报错
+    public FirstDesignView(Context context) {
+        super(context);
+    }
+
+    public FirstDesignView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FirstDesignView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension( measureWidth(widthMeasureSpec), measureHeight(heightMeasureSpec));
+    }
+
+    private int measureWidth( int measureSpec ){
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY){
+            result = specSize;
+        }else {
+            result = 200;
+            if (specMode == MeasureSpec.AT_MOST){
+                result = Math.min(result, specSize);
+            }
+        }
+
+        return result;
+    }
+
+    private int measureHeight( int measureSpec ){
+        int result = 0;
+        int specMode = MeasureSpec.getMode(measureSpec);
+        int specSize = MeasureSpec.getSize(measureSpec);
+
+        if (specMode == MeasureSpec.EXACTLY){
+            result = specSize;
+        }else {
+            result = 200;
+            if (specMode == MeasureSpec.AT_MOST){
+                result = Math.min(result, specSize);
+            }
+        }
+
+        return result;
+    }
+
+}
